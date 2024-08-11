2304ce669be033c4554ab601413f3a7f238540e7
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2024-08-11 18:54:43 +0800

    ✅test: add a unitset test file

---
+++ test/ToolTest.py
@@ -0,0 +1,18 @@
+import unittest
+from EasyClipToolMainFrame import ConfigTool
+
+
+class MyTestCase(unittest.TestCase):
+    def test_timeStrIsValid(self):
+        self.assertTrue(ConfigTool.timeStrIsValid("00 00"))
+        self.assertTrue(ConfigTool.timeStrIsValid("00:00"))
+        self.assertTrue(ConfigTool.timeStrIsValid("00：00"))
+        self.assertTrue(ConfigTool.timeStrIsValid("23 59"))
+        self.assertTrue(ConfigTool.timeStrIsValid("11:11:23"))
+        self.assertTrue(ConfigTool.timeStrIsValid("11：11 23"))
+        self.assertFalse(ConfigTool.timeStrIsValid("错误的"))
+        self.assertFalse(ConfigTool.timeStrIsValid("daigoaf"))
+
+
+if __name__ == '__main__':
+    unittest.main()
